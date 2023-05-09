36f7251f8eb7a120e7da8c0ce85dbdc2816f99ad
Fazel Matt <mahdifazeli@live.com>
2023-05-10 02:51:03 +0330

    mui-added with emotion cache for ssr

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import createEmotionCache from "@/utility/createEmotionCache";
+import theme from "@/utility/theme";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider } from "@mui/material";
+
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
+    return <CacheProvider value={emotionCache}><ThemeProvider theme={theme}><Component {...pageProps} />  </ThemeProvider></CacheProvider>
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Switch from "@mui/material/Switch";
+
+const label = { inputProps: { "aria-label": "Switch demo" } };
+
+export default function Home() {
+ return (
+   <div >
+     <div>
+       <span>With default Theme:</span>
+     </div>
+     <Switch {...label} defaultChecked />
+     <Switch {...label} />
+     <Switch {...label} disabled defaultChecked />
+   </div>
+ );
+}
