dded1487f232183848fa23210d1d77e82a1e905c
matt <mahdifazeli@live.com>
2023-06-23 22:54:00 +0430

    fix

--- src/prisma/service/driver/get.ts
+++ src/prisma/service/driver/get.ts
@@ -4,10 +4,23 @@ export async function getDriver(search: string) {
     try {
         const driverFromDb = await prisma.driver.findUnique({
             where: {
-                mobile: search,
+                OR: [
+                    {
+                        mobile: {
+                            contains: search
+                        }
+
+                    },
+                    {
+                        pelak: {
+                            contains: search
+                        }
+                    },
+                ],
             },
 
         })
+        console.log(driverFromDb)
         return { data: driverFromDb }
     } catch (error) {
         return error
