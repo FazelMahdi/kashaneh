88da8fc0059050ff8a5abf99b5b6d2d1314210e8
matt <mahdifazeli@live.com>
2023-07-02 03:14:35 +0430

    add global loading and add custom hook for loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,20 @@
-'use client'
+import "@/core/theme/scss/index.scss";
+import "@/styles/globals.css";
 
-import '@/core/theme/scss/index.scss';
-import '@/styles/globals.css';
-
-import DefaultLayout from '@/layouts/default';
-import EmptyLayout from '@/layouts/empty';
+import DefaultLayout from "@/layouts/default";
+import EmptyLayout from "@/layouts/empty";
 import createEmotionCache from "@/utility/createEmotionCache";
 import theme from "@/utility/theme";
 import { CacheProvider } from "@emotion/react";
 import { ThemeProvider } from "@mui/material";
+import { useRouter } from "next/router";
+import useLoading from "@/components/utils/useLoading";
+import Image from "next/image";
 
 const layouts = {
-    default: DefaultLayout,
-    empty: EmptyLayout
-}
+  default: DefaultLayout,
+  empty: EmptyLayout,
+};
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -36,8 +37,43 @@ const clientSideEmotionCache = createEmotionCache();
 //     };
 // }, [router]);
 
-export default function App({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
-    const Layout = layouts[Component.layout ?? 'default'] || ((children) => <>{children}</>);
+export default function App({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) {
+  const router = useRouter();
+  const isLoading = useLoading();
+  const Layout =
+    layouts[Component.layout ?? "default"] || ((children) => <>{children}</>);
 
-    return <CacheProvider value={emotionCache}><ThemeProvider theme={theme}> <Layout><Component {...pageProps} /> </Layout> </ThemeProvider></CacheProvider>
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        {" "}
+        <Layout>
+          {isLoading ? (
+            <div className="text-center mt-10">
+              <div className="mx-auto text-center rounded-xl py-10">
+                <Image
+                  src="/loading.gif"
+                  alt="کاشانه"
+                  width={80}
+                  height={80}
+                  priority
+                  className="mx-auto mb-3"
+                />
+                <p className="font-extrabold text-center text-orange-800 mb-2">
+                  کارخانه آجر کاشانه
+                </p>
+                <span className="font-light">در حال بارگزاری</span>{" "}
+              </div>
+            </div>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </Layout>{" "}
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
