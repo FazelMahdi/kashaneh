14057c454567572446084297dca498e1b8e92d4d
matt <mahdifazeli@live.com>
2023-07-03 21:46:47 +0430

    fix

--- src/core/util/storage.ts
+++ src/core/util/storage.ts
@@ -1,26 +1,14 @@
-import * as CryptoJS from 'crypto-js';
 import Cookies from 'js-cookie';
 
-const hash = process.env.NEXT_PUBLIC_APP_NAME;
-
-export function encrypt(data) {
-    return CryptoJS.AES.encrypt(data, hash).toString();
-}
-
-export function decrypt(ciphertext) {
-    const bytes = CryptoJS.AES.decrypt(ciphertext, hash);
-    return bytes.toString(CryptoJS.enc.Utf8);
-}
-
 export function setItem(key, value) {
-    return Cookies.set(key, encrypt(JSON.stringify(value)));
+    return Cookies.set(key, value);
 }
 
 export async function getItem(key) {
     try {
         const data = await Cookies.get(key);
         if (data) {
-            return JSON.parse(decrypt(data));
+            return data;
         }
         return null;
     } catch {
